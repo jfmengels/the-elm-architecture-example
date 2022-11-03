70052478b2d21e95f833b7fcf06990c71850b24c
Jeroen Engels <jfm.engels@gmail.com>
2022-11-03 13:34:40 +0100

    Separate state for counters

--- src/Main.elm
+++ src/Main.elm
@@ -7,46 +7,68 @@ import Html.Events exposing (onClick)
 
 
 type alias Model =
-    { count : Int }
+    { count1 : Int
+    , count2 : Int
+    , count3 : Int
+    }
 
 
 initialModel : Model
 initialModel =
-    { count = 0 }
+    { count1 = 0
+    , count2 = 0
+    , count3 = 0
+    }
 
 
 type Msg
-    = Increment
-    | Decrement
+    = Increment1
+    | Decrement1
+    | Increment2
+    | Decrement2
+    | Increment3
+    | Decrement3
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment ->
-            { model | count = model.count + 1 }
+        Increment1 ->
+            { model | count1 = model.count1 + 1 }
 
-        Decrement ->
-            { model | count = model.count - 1 }
+        Decrement1 ->
+            { model | count1 = model.count1 - 1 }
+
+        Increment2 ->
+            { model | count2 = model.count2 + 1 }
+
+        Decrement2 ->
+            { model | count2 = model.count2 - 1 }
+
+        Increment3 ->
+            { model | count3 = model.count3 + 1 }
+
+        Decrement3 ->
+            { model | count3 = model.count3 - 1 }
 
 
 view : Model -> Html Msg
 view model =
     div [ Attr.style "display" "flex" ]
         [ div []
-            [ button [ onClick Increment ] [ text "+1" ]
-            , div [] [ text <| String.fromInt model.count ]
-            , button [ onClick Decrement ] [ text "-1" ]
+            [ button [ onClick Increment1 ] [ text "+1" ]
+            , div [] [ text <| String.fromInt model.count1 ]
+            , button [ onClick Decrement1 ] [ text "-1" ]
             ]
         , div []
-            [ button [ onClick Increment ] [ text "+1" ]
-            , div [] [ text <| String.fromInt model.count ]
-            , button [ onClick Decrement ] [ text "-1" ]
+            [ button [ onClick Increment2 ] [ text "+1" ]
+            , div [] [ text <| String.fromInt model.count2 ]
+            , button [ onClick Decrement2 ] [ text "-1" ]
             ]
         , div []
-            [ button [ onClick Increment ] [ text "+1" ]
-            , div [] [ text <| String.fromInt model.count ]
-            , button [ onClick Decrement ] [ text "-1" ]
+            [ button [ onClick Increment3 ] [ text "+1" ]
+            , div [] [ text <| String.fromInt model.count3 ]
+            , button [ onClick Decrement3 ] [ text "-1" ]
             ]
         ]
 
